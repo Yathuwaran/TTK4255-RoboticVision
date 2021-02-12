5e7e98d8ed0b5181f10384be2c58755077330a6c
yathuwaran <yathuwar@stud.ntnu.no>
2021-02-12 19:03:11 +0100

    A4 Task 2 done

---
+++ A4/python/common.py
@@ -0,0 +1,132 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def reprojection_error(uv_gt, uv_pred):
+    e = np.zeros([uv_gt.shape[1],1])
+    for i in range(uv_gt.shape[1]):
+        e[i] = np.sqrt((uv_gt[:2,i]- uv_pred[:2,i]).T@ (uv_gt[:2,i]- uv_pred[:2,i]))
+
+    return e
+def estimate_H(xy, XY):
+    # Tip: U,s,VT = np.linalg.svd(A) computes the SVD of A.
+    # The column of V corresponding to the smallest singular value
+    # is the last column, as the singular values are automatically
+    # ordered by decreasing magnitude. However, note that it returns
+    # V transposed.
+
+
+    n = XY.shape[0]
+    A = np.empty([0,9])
+    for i in range(n):
+        A1 = np.array([XY[i,0], XY[i,1], 1, 0, 0, 0, -XY[i,0]*xy[i,0], -XY[i,1]*xy[i,0], -xy[i,0] ])
+        A2 = np.array([ 0, 0, 0, XY[i,0], XY[i,1], 1, -XY[i,0]*xy[i,1], -XY[i,1]*xy[i,1], -xy[i,1] ])
+
+        A = np.vstack([A,A1])
+        A = np.vstack([A, A2])
+
+    #Solve Ah = 0 using SVD (Single-value-decomposition)
+
+    _,_,V = np.linalg.svd(A, full_matrices = True, compute_uv=True)
+    V = V.T
+    h = V[:,-1] # Last column of V
+    H = np.reshape(h,[3,3])
+
+    return H
+
+def decompose_H(H):
+    # Tip: Use np.linalg.norm to compute the Euclidean length
+
+    T1 = np.eye(4) # Placeholder, replace with your implementation
+    T2 = np.eye(4) # Placeholder, replace with your implementation
+
+    k = np.linalg.norm(H[:, 0])
+    r1 = H[:, 0] / k
+    r2 = H[:, 1] / k
+    r3 = np.cross(r1, r2)
+    # Extract translation vector
+    t = H[:, 2] / k
+    T1[0:3, 0:4] = np.column_stack((r1, r2, r3, t))
+
+    # Extract rotation
+    r1 = H[:, 0] / -k
+    r2 = H[:, 1] / -k
+    r3 = np.cross(r1, r2)
+    # Extract translation vector
+    t = H[:, 2] / -k
+    T2[0:3, 0:4] = np.column_stack((r1, r2, r3, t))
+
+    T1 = np.eye(4) # Placeholder, replace with your implementation
+    T2 = np.eye(4)
+
+    return T1, T2
+
+
+def closest_rotation_matrix(Q):
+    R = Q # Placeholder
+    return R
+
+def project(K, X):
+    """
+    Computes the pinhole projection of an (3 or 4)xN array X using
+    the camera intrinsic matrix K. Returns the dehomogenized pixel
+    coordinates as an array of size 2xN.
+    """
+    uvw = K@X[:3,:]
+    uvw /= uvw[2,:]
+    return uvw[:2,:]
+
+def draw_frame(K, T, scale=1):
+    """
+    Visualize the coordinate frame axes of the 4x4 object-to-camera
+    matrix T using the 3x3 intrinsic matrix K.
+
+    Control the length of the axes by specifying the scale argument.
+    """
+    X = T @ np.array([
+        [0,scale,0,0],
+        [0,0,scale,0],
+        [0,0,0,scale],
+        [1,1,1,1]])
+    u,v = project(K, X)
+    plt.plot([u[0], u[1]], [v[0], v[1]], color='red') # X-axis
+    plt.plot([u[0], u[2]], [v[0], v[2]], color='green') # Y-axis
+    plt.plot([u[0], u[3]], [v[0], v[3]], color='blue') # Z-axis
+
+def generate_figure(fig, image_number, K, T, uv, uv_predicted, XY):
+
+    fig.suptitle('Image number %d' % image_number)
+
+    #
+    # Visualize reprojected markers and estimated object coordinate frame
+    #
+    I = plt.imread('../data/image%04d.jpg' % image_number)
+    plt.subplot(121)
+    plt.imshow(I)
+    draw_frame(K, T, scale=4.5)
+    plt.scatter(uv[0,:], uv[1,:], color='red', label='Detected')
+    plt.scatter(uv_predicted[0,:], uv_predicted[1,:], marker='+', color='yellow', label='Predicted')
+    plt.legend()
+    plt.xlim([0, I.shape[1]])
+    plt.ylim([I.shape[0], 0])
+
+    #
+    # Visualize scene in 3D
+    #
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    ax.plot(XY[0,:], XY[1,:], np.zeros(XY.shape[1]), '.') # Draw markers in 3D
+    pO = np.linalg.inv(T)@np.array([0,0,0,1]) # Compute camera origin
+    pX = np.linalg.inv(T)@np.array([6,0,0,1]) # Compute camera X-axis
+    pY = np.linalg.inv(T)@np.array([0,6,0,1]) # Compute camera Y-axis
+    pZ = np.linalg.inv(T)@np.array([0,0,6,1]) # Compute camera Z-axis
+    plt.plot([pO[0], pZ[0]], [pO[1], pZ[1]], [pO[2], pZ[2]], color='blue') # Draw camera Z-axis
+    plt.plot([pO[0], pY[0]], [pO[1], pY[1]], [pO[2], pY[2]], color='green') # Draw camera Y-axis
+    plt.plot([pO[0], pX[0]], [pO[1], pX[1]], [pO[2], pX[2]], color='red') # Draw camera X-axis
+    ax.set_xlim([-40, 40])
+    ax.set_ylim([-40, 40])
+    ax.set_zlim([-25, 25])
+    ax.set_xlabel('X')
+    ax.set_zlabel('Y')
+    ax.set_ylabel('Z')
+
+    plt.tight_layout()
---
+++ A4/python/main.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from common import *
+
+K           = np.loadtxt('../data/K.txt')
+detections  = np.loadtxt('../data/detections.txt')
+XY          = np.loadtxt('../data/XY.txt').T
+n_total     = XY.shape[1] # Total number of markers (= 24)
+
+fig = plt.figure(figsize=plt.figaspect(0.35))
+XY = np.vstack((XY, np.ones(n_total)))
+#for image_number in range(23): # Use this to run on all images
+for image_number in [4]: # Use this to run on a single image
+
+    # Load data
+    # valid : Boolean mask where valid[i] is True if marker i was detected
+    #     n : Number of successfully detected markers (<= n_total)
+    #    uv : Pixel coordinates of successfully detected markers
+    valid = detections[image_number, 0::3] == True
+    uv = np.vstack((detections[image_number, 1::3], detections[image_number, 2::3]))
+    uv = uv[:, valid]
+    n = uv.shape[1]
+
+    # Tip: The 'valid' array can be used to perform Boolean array indexing,
+    # e.g. to extract the XY values of only those markers that were detected.
+    # Use this when calling estimate_H and when computing reprojection error.
+
+    # Tip: Helper arrays with 0 and/or 1 appended can be useful if
+    # you want to replace for-loops with array/matrix operations.
+    uv = np.vstack((uv, np.ones(n)))
+    
+    # XY01 = np.vstack((XY, np.zeros(n_total), np.ones(n_total)))
+
+    
+    xy = np.linalg.inv(K)@uv
+    xy = xy/xy[2,:]
+    print(XY.shape, xy.shape)
+    #print(xy[3,0])          # TASK: Compute calibrated image coordinates
+    H = estimate_H(xy.T, XY[:, valid].T)   # TASK: Implement this function
+    uv_hat = (K@H@XY)
+    
+    uv_from_H = (uv_hat/uv_hat[2,:]).T # TASK: Compute predicted pixel coordinates using H
+    
+    T1,T2 = decompose_H(H) # TASK: Implement this function
+
+    T = T1 # TASK: Choose solution (try both T1 and T2 for Task 3.1, but choose automatically for Task 3.2)
+    error = reprojection_error(uv, uv_from_H.T)
+    print("Max error: ", np.max(error))
+    print("Min error: ", np.min(error))
+    print("Avg error: ", np.average(error))
+    # The figure should be saved in the data directory as out0000.png, etc.
+    # NB! generate_figure expects the predicted pixel coordinates as 'uv_from_H'.
+    plt.clf()
+    generate_figure(fig, image_number, K, T, uv, uv_from_H.T, XY)
+    plt.savefig('../data/out%04d.png' % image_number)
